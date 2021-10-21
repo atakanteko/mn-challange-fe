3e1ca4f843dbc19e50cdf34d1a9dcd2c04c16812
atakanteko <atakantekoglu@gmail.com>
2021-10-21 03:48:55 +0300

    Test and Git Action

---
+++ cypress/integration/todo_app.js
@@ -0,0 +1,22 @@
+describe('Todo app', function() {
+
+  beforeEach(function() {
+    cy.request('GET', 'https://mnchallenge.herokuapp.com/reset')
+    const todo = {
+      content:"Hello, this is a new content."
+    }
+    cy.request('POST','https://mnchallenge.herokuapp.com/api/todos/',todo)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Moda Nisa Assignment - ToDo List')
+  })
+
+  it('a new todo from cypress can be created', function() {
+    cy.get('input').type('another todo cypress input')
+    cy.contains('Add').click()
+    cy.contains('another todo cypress input')
+    })
+
+ })
--- src/services/todos.js
+++ src/services/todos.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 require('dotenv').config();
 
-const baseUrl = `${process.env.REACT_APP_NOT_SECRET_CODE}/api/todos` 
+const baseUrl = `https://mnchallenge.herokuapp.com/api/todos` 
 
 
 const getAll = () => {
